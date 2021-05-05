a6ff470cc22a7c31afe94329253e44ee0d5873fc
Mateusz Kowalewski <mkowalewski@clickmeeting.com>
2021-05-06 00:44:23 +0200

    fix

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Tpay;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
